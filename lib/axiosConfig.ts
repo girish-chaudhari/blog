9cb7ef3d6c9e251f3f1618701955377532565c9e
girish.chaudhari <girish.chaudhari@visaero.com>
2023-06-22 10:15:45 +0530

    resolved

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axiosIntance from 'axios';
 
-export let baseURL = process.env.NODE_ENV === 'production' ? "https://dev-blog-girish-girishvisaero.vercel.app/": "http://localhost:8080/api"
+export let baseURL = process.env.NODE_ENV === 'production' ? "https://dev-blog-girish-girishvisaero.vercel.app/api": "http://localhost:8080/api"
 
 const axios = axiosIntance.create({
   baseURL
